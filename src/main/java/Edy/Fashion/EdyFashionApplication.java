f1de5c4de431e36b191dd2950cfba3e8e5965ca4
Thiago Dias Jardim <thiago2020dias@gmail.com>
2025-12-15 11:08:07 -0300

    Implementação de Springboot, Java classes, atualização do código

---
+++ src/main/java/Edy/Fashion/EdyFashionApplication.java
@@ -0,0 +1,13 @@
+package Edy.Fashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdyFashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdyFashionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Edy/Fashion/EdyFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package Edy.Fashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdyFashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
